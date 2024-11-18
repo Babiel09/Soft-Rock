821ce04e253eba305932f5cc769d819ee4a07c3b
Gabriel Castro <estudosbabiel@gmail.com>
2024-11-18 19:53:20 -0300

    feat> Add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dbConnection from './configs/databse.js';
 import { routes } from './routes/route.js';
+import cors from 'cors';
+
 
 const connection = await dbConnection();
 
@@ -12,8 +14,14 @@ connection.once("open", ()=>{
     console.log("Conexão com o banco de dados estabelecida");
 });
 
+    const corsOption =  {
+        origin: process.env.FRONT_PORT,
+        methods: ["GET, POST, PUT, PATCH, DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    } 
 
 const app = express();
+app.use(cors(corsOption))
 routes(app);
 
 
